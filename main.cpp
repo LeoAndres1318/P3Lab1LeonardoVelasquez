697200c0ac60f1731c5e023f8bf9fc9487983775
LeoAndres1318 <56442638+LeoAndres1318@users.noreply.github.com>
2021-01-22 15:24:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,32 @@ using namespace  std;
 
 int main(int argc, char** argv) {
 	int opcion 0=;
-	cout<<"Ingrese Opcion: ";
+	cout<<"Ingrese Opcion 1 o 2: ";
 	cin>>opcion;
 	if(opcion ==1){
-		cout<<"Ingrese Opcion: ";
+	cout<<"Ingrese Opcion: ";
 		int n =0;
 		if(opcion==1){
+			cout<<"Ejercicio 1";
 			cout<<"Ingrese n: ";
 			int n=0;
 			cin>>n;
 			int acum=0;
 			for(int i=0; int <=n; i ++){
-				acum+=i*pow(i+1,2); 
+				acum+=i*pow(i+1,2);
+				cout<< acum; 
 			}
-		}
-		cout<< acum;
+		}if(opcion==2){
+	        cout<<"Ejercicio 2";
+	        cout<<"Ingrese n: ";
+	        float x=0;
+	        cin>>x;
+	        float acumulador=0;
+	        acumulador+=1/(1+exp(x));
+	        cout<<"Resultado: " <<acum<<endl;		
+}
+		
+	        
 	}
 	return 0;
 	
